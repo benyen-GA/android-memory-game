2e70092eb160b3dc33b45b9b847c19f26b47bbb7
Lunar-lau <laushutting@gmail.com>
2025-06-22 13:47:11 +0800

    dummy play activity

--- Android/app/src/main/java/iss/nus/edu/sg/memory_game/fragment/PlayFragment.kt
+++ Android/app/src/main/java/iss/nus/edu/sg/memory_game/fragment/PlayFragment.kt
@@ -5,13 +5,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.GridLayout
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import iss.nus.edu.sg.memory_game.R
 
 class PlayFragment : Fragment() {
+    private lateinit var cardGrid: GridLayout
+    private lateinit var imageList: List<Int>
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_play, container, false)
+        val view = inflater.inflate(R.layout.fragment_play, container, false)
+        cardGrid = view.findViewById(R.id.cardGrid)
+
+        showCardBacks()
+
+        return view
+    }
+
+    private fun showCardBacks(){
+        cardGrid.removeAllViews()
+
+        val columns = 3
+
+        val baseImg = listOf(
+            R.drawable.card_1_test,
+            R.drawable.card_2_test,
+            R.drawable.card_3_test,
+            R.drawable.card_4_test,
+            R.drawable.card_5_test,
+            R.drawable.card_6_test
+        )
+
+        val duplicatedImg = baseImg + baseImg
+
+        imageList = duplicatedImg.shuffled()
+
+        for (i in 0..11){
+            val card = ImageView(requireContext()).apply {
+                setImageResource(R.drawable.card_back)
+                scaleType = ImageView.ScaleType.CENTER_CROP
+                layoutParams = GridLayout.LayoutParams().apply {
+                    width = 0
+                    height = 0
+                    rowSpec = GridLayout.spec(i / columns, 1f)
+                    columnSpec = GridLayout.spec(i % columns, 1f)
+                    setMargins(5,5,5,5)
+                }
+                setOnClickListener {
+                    this.setImageResource(imageList[i])
+                }
+            }
+            cardGrid.addView(card)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

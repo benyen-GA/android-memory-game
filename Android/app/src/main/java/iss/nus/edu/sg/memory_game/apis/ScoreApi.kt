ed02089f01305321f25d195ec2452be4e844e4ed
Lunar-lau <laushutting@gmail.com>
2025-07-04 23:55:02 +0800

    Add ScoreApi,ScoreRequest; update RetrofiClient, PlayFrament.

---
+++ Android/app/src/main/java/iss/nus/edu/sg/memory_game/apis/ScoreApi.kt
@@ -0,0 +1,11 @@
+package iss.nus.edu.sg.memory_game.apis
+
+import retrofit2.Call
+import iss.nus.edu.sg.memory_game.dao.ScoreRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ScoreApi {
+    @POST("api/score/add")
+    fun addScore(@Body scoreRequest: ScoreRequest): Call<Void>
+}
---
+++ Android/app/src/main/java/iss/nus/edu/sg/memory_game/dao/ScoreRequest.kt
@@ -0,0 +1,6 @@
+package iss.nus.edu.sg.memory_game.dao
+
+data class ScoreRequest (
+    val userId: String,
+    val time: Int
+)
